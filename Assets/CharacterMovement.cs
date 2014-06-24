4f2f51a4afe27ff3f279fd9f44426c0fd32be9ef
I Putu Pradnyana <pradnyana12@mhs.if.its.ac.id>
2014-06-24 20:46:09 +0700

    Character Movement Fix 3
    Sekarang inverted movement sudah bisa digunakan

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -12,7 +12,6 @@ public class CharacterMovement : MonoBehaviour {
 	//Movement of the character, one must be straight and one must be inverted
 	public enum Movement { Straight, Inverted};
 	public Movement moveDir;
-	private int Move;
 	//Map of the area
 	private Grid.Type[,] map;
 	//Get the current position
@@ -35,53 +34,80 @@ public class CharacterMovement : MonoBehaviour {
 		now.y = StartY;
 		//Put he character to the right position
 		this.transform.position = ( new Vector3( StartX + 0.5f, StartY + 0.5f, 0));
-		
-		//Adjust the movement
-		switch(moveDir){
-		case Movement.Straight:
-			Move = 1;
-			break;
-		case Movement.Inverted:
-			Move = -1;
-			break;
-		}
+
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		if (cooldown > 0) {
-						cooldown -= Time.deltaTime;
+			cooldown -= Time.deltaTime;
 		} else {
 			if (Input.GetKey (KeyCode.UpArrow)) {
 				Debug.Log ("(" + now.x + "," + now.y + ")");
-				if (now.y < GameController.mapHeight - 1 && map [now.x, now.y + 1] != Grid.Type.bottom && map [now.x, now.y + 1] != Grid.Type.bottomRight) {
-					this.transform.Translate (0, 1f * Move, 0);
-					now.y++;
-					cooldown = MAX_COOLDOWN;
+				if(moveDir == Movement.Straight){
+					MoveUp ();
+				}
+				else{
+					MoveDown ();
 				}
 			} else if (Input.GetKey (KeyCode.DownArrow)) {
 				Debug.Log ("(" + now.x + "," + now.y + ")");
-				if (now.y > 0 && map [now.x, now.y] != Grid.Type.bottom && map [now.x, now.y] != Grid.Type.bottomRight) {
-					this.transform.Translate (0, -1f * Move, 0);
-					now.y--;
-					cooldown = MAX_COOLDOWN;
+				if(moveDir == Movement.Straight){
+					MoveDown();
+				}
+				else{
+					MoveUp ();
 				}
 			} else if (Input.GetKey (KeyCode.RightArrow)) {
 				Debug.Log ("(" + now.x + "," + now.y + ")");
-				if (now.x < GameController.mapWidth - 1 && map [now.x, now.y] != Grid.Type.right && map [now.x, now.y] != Grid.Type.bottomRight) {
-					this.transform.Translate (1f * Move, 0, 0);
-					now.x++;
-					cooldown = MAX_COOLDOWN;
-				} 
+				if(moveDir == Movement.Straight){
+					MoveRight ();
+				}
+				else{
+					MoveLeft ();
+				}
 			} else if (Input.GetKey (KeyCode.LeftArrow)) {
 				Debug.Log ("(" + now.x + "," + now.y + ")");
-				if (now.x > 0 && map [now.x - 1, now.y] != Grid.Type.right && map [now.x - 1, now.y] != Grid.Type.bottomRight) {
-					this.transform.Translate (-1f * Move, 0, 0);
-					now.x--;
-					cooldown = MAX_COOLDOWN;
+				if(moveDir == Movement.Straight){
+					MoveLeft ();
+				}
+				else{
+					MoveRight ();
 				}
 			}
+		}
+	}
+
+	void MoveRight(){
+		if (now.x < GameController.mapWidth - 1 && map [now.x, now.y] != Grid.Type.right && map [now.x, now.y] != Grid.Type.bottomRight) {
+			this.transform.Translate (1f, 0, 0);
+			now.x++;
+			cooldown = MAX_COOLDOWN;
+		}
+	}
+
+	void MoveLeft(){
+		if (now.x > 0 && map [now.x - 1, now.y] != Grid.Type.right && map [now.x - 1, now.y] != Grid.Type.bottomRight) {
+			this.transform.Translate (-1f, 0, 0);
+			now.x--;
+			cooldown = MAX_COOLDOWN;
+		}
+	}
+
+	void MoveUp(){
+		if (now.y < GameController.mapHeight - 1 && map [now.x, now.y + 1] != Grid.Type.bottom && map [now.x, now.y + 1] != Grid.Type.bottomRight) {
+			this.transform.Translate (0, 1f, 0);
+			now.y++;
+			cooldown = MAX_COOLDOWN;
+		}
+	}
+
+	void MoveDown(){
+		if (now.y > 0 && map [now.x, now.y] != Grid.Type.bottom && map [now.x, now.y] != Grid.Type.bottomRight) {
+			this.transform.Translate (0, -1f, 0);
+			now.y--;
+			cooldown = MAX_COOLDOWN;
 		}
 	}
 }
